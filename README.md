a33d9a7af5187149f87929b2178e8bdc5a12d85a
infeng <fzhihao@outlook.com>
2017-12-19 11:55:00 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# 工具
+
+> support nodejs v8.0.0 and higher.
+
+## 创建model
+
+```bash
+node tools model <name>
+```
+
+### options
+
+* `--no-api` -- create model without api
+* `--list` -- create model with list
+* `--apiPath` -- set api path
+
+### usage
+
+```bash
+node tools model base
+```
+
+```bash
+node tools model operator --list --apiPath /system/getOperatorList
+```
---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+let model = require('../lib/model');
+program
+.command('model <modelPath>')
+.description('create model')
+.usage('test')
+.option('--no-api', 'create model without api')
+.option('--list', 'create model with list')
+.option('--apiPath [value]', 'set api path')
+.action(function(modelPath, options){
+  console.log(`create model ${modelPath}`);
+  model(modelPath, options);
+});
+
+program.parse(process.argv);
